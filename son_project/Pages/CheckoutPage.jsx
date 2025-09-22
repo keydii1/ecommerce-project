c093304cefe55fbb0f8fc0da2974fb431399e70d
Hồ Hoàng Sơn <23521352@gm.uit.edu.vn>
2025-09-22 21:51:52 +0700

    code delivery option

--- son_project/Pages/CheckoutPage.jsx
+++ son_project/Pages/CheckoutPage.jsx
@@ -1,9 +1,20 @@
 import { Header } from "./Header"
+import dayjs from 'dayjs'
 import './CheckoutPage.css'
 import { formatMoney } from "../src/utils/money"
+import axios from 'axios';
+import {useState, useEffect} from 'react'
 export function CheckoutPage({cart}) {
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+  useEffect(() => {
+    axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
+    .then((response) => {
+      setDeliveryOptions(response.data);
+    })
+  }, []);
   return (
     <>
+    <title>Checkout</title>
     <Header cart={cart} />
     <div className="checkout-page">
       <div className="page-title">Review your order</div>
@@ -45,45 +56,31 @@ export function CheckoutPage({cart}) {
                 <div className="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div className="delivery-option">
-                  <input type="radio" checked
+                {deliveryOptions.map((deliveryOption) => {
+                  let priceString  = 'FREE SHIPPING';
+
+                  if(deliveryOption.priceCents > 0){
+                    priceString= `${formatMoney(deliveryOption.priceCents)} - Shipping`
+                  }
+                  return (
+                    <>
+                <div key={deliveryOption.id} className="delivery-option">
+                  <input type="radio" checked={deliveryOption.id === cartItem.deliveryOptionId}
                     className="delivery-option-input"
-                    name="delivery-option-1" />
+                    name={`delivery-option-${cartItem.productId}`} />
                   <div>
                     <div className="delivery-option-date">
-                      Tuesday, June 21
+                      {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
                     </div>
                     <div className="delivery-option-price">
-                      FREE Shipping
+                      {priceString}
                     </div>
                   </div>
                 </div>
-                <div className="delivery-option">
-                  <input type="radio"
-                    className="delivery-option-input"
-                    name="delivery-option-1" />
-                  <div>
-                    <div className="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div className="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div className="delivery-option">
-                  <input type="radio"
-                    className="delivery-option-input"
-                    name="delivery-option-1" />
-                  <div>
-                    <div className="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div className="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+                    </>
+                  )
+                })}
+
               </div>
             </div>
           </div>
