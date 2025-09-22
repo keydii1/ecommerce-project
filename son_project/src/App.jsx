6d9e5082279c4533e889edaef22d78c5af27770d
Hồ Hoàng Sơn <23521352@gm.uit.edu.vn>
2025-09-22 15:52:32 +0700

    change proxy of product into cart becasue i confuse

--- son_project/src/App.jsx
+++ son_project/src/App.jsx
@@ -11,7 +11,7 @@ import { useEffect,useState } from "react";
     const [cart,setCart] = useState([]);
 
     useEffect(() => {
-    axios.get('/api/products')
+    axios.get('/api/cart-items')
   .then((response) => {
     setCart(response.data);
   })
